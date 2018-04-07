d8aee2327500a87a082eff9b5f278fc7bede4a32
Pascal <wermail2002-kap@yahoo.de>
2018-04-07 22:28:09 +0200

    refactor(Webhooks.Util): use function clauses for content type

--- lib/webhooks/util.ex
+++ lib/webhooks/util.ex
@@ -12,14 +12,15 @@ defmodule Webhooks.Util do
     - Halts the connection
     - Returns the connection
   """
-  def respond(conn, status, data \\ "") do
-    {content_type, data} =
-      if is_map(data) do
-        {"application/json", Poison.encode!(data)}
-      else
-        {"plain/text", data}
-      end
+  def respond(conn, status, data \\ "")
 
+  def respond(conn, status, data) when is_bitstring(data),
+    do: do_respond(conn, status, "plain/text", data)
+
+  def respond(conn, status, data) when is_map(data),
+    do: do_respond(conn, status, "application/json", Poison.encode!(data))
+
+  defp do_respond(conn, status, content_type, data) do
     conn
     |> put_resp_content_type(content_type)
     |> send_resp(status, data)
