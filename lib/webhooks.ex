62cadf22092fcc48520576365b9e36d3ca537750
Pascal <wermail2002-kap@yahoo.de>
2018-04-08 09:23:35 +0200

    feat(Webhooks.Application): allow redis options to be overriden

--- lib/webhooks.ex
+++ lib/webhooks.ex
@@ -1,13 +1,19 @@
 defmodule Webhooks.Application do
   @moduledoc """
     Small application listening for webhooks notifications
+
+    You can configure the redis connection by specifying valid
+    `Redix.start_link/2` options under the environment key `:redis`.
+    > Defaults to `host: "127.0.0.1", port: 6379`
   """
 
   use Application
 
   def start(_type, _args) do
+    redis_opts = Application.get_env(:webhooks, :redis, host: "127.0.0.1", port: 6379)
+
     children = [
-      {Redix, [[host: "127.0.0.1", port: 6379], [name: :redix]]},
+      {Redix, [redis_opts, [name: :redix]]},
       Plug.Adapters.Cowboy.child_spec(
         :http,
         Webhooks.Routers,
